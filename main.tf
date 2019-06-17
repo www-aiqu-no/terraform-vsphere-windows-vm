26d2a8ecba849e838d631180ba329d23c2c43af2
Rune Rønneseth <rune.ronneseth@aiqu.no>
2019-06-17 15:39:56 +0200

    Update main.tf
    Customization timeout was placed in wrong bracket. Moved to correct place & set it to '-1' (disabled)

--- main.tf
+++ main.tf
@@ -1,30 +1,41 @@
-## -----------------------------------------------------------------------------
+## =============================================================================
 #                                                Deploy VM from vSphere Template
-## -----------------------------------------------------------------------------
+## =============================================================================
 resource "vsphere_virtual_machine" "vm" {
   count = "${length(var.hosts)}"
+# ------------------------------------------------------------------------------
 
-  annotation = "Deployed by HashiCorp Terraform"
+  annotation                  = "Deployed by HashiCorp Terraform"
   wait_for_guest_net_timeout  = 0
   wait_for_guest_net_routable = false
+
+# ------------------------------------------------------------------------------
 
   host_system_id   = "${data.vsphere_host.hosts.*.id[count.index]}"
   resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
   datastore_id     = "${data.vsphere_datastore.datastore.id}"
 
+# ------------------------------------------------------------------------------
+
   guest_id  = "${data.vsphere_virtual_machine.template.guest_id}"
   firmware  = "${data.vsphere_virtual_machine.template.firmware}"
   scsi_type = "${data.vsphere_virtual_machine.template.scsi_type}"
+
+# ------------------------------------------------------------------------------
 
   name     = "${var.name}${count.index}"
   folder   = "${var.vsphere_folder}"
   memory   = "${var.ram_mb}"
   num_cpus = "${var.cpu}"
 
+# ------------------------------------------------------------------------------
+
   cpu_hot_add_enabled    = true
   cpu_hot_remove_enabled = true
   memory_hot_add_enabled = true
   boot_delay             = "${var.vsphere_boot_delay}"
+
+# ------------------------------------------------------------------------------
 
   disk {
     label            = "disk0"
@@ -39,17 +50,21 @@ resource "vsphere_virtual_machine" "vm" {
     }"
   }
 
+# ------------------------------------------------------------------------------
+
   network_interface {
     adapter_type = "${data.vsphere_virtual_machine.template.network_interface_types.0}"
     network_id   = "${data.vsphere_network.network.id}"
   }
 
+# ------------------------------------------------------------------------------
+
   clone {
     linked_clone  = false
     template_uuid = "${data.vsphere_virtual_machine.template.id}"
-    timeout       = 60
     customize {
-      ipv4_gateway    = "${var.ipv4_gateway}"
+      timeout      = -1
+      ipv4_gateway = "${var.ipv4_gateway}"
       network_interface {
         dns_domain      = "${var.dns_domain}"
         dns_server_list = "${var.dns_servers}"
@@ -69,4 +84,7 @@ resource "vsphere_virtual_machine" "vm" {
       }
     }
   }
+
+# ------------------------------------------------------------------------------
+
 }
